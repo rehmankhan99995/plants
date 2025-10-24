88c8d9e58f81eed42627e92ca0eb6fbbc8e4b41f
Abdul Rehman <rehman00004khan@gmail.com>
2025-10-24 22:08:46 +0500

    fix the navbar link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/ui/navbar";
+import Navbar from "@/components/ui/Navbar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
 
